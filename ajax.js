817f673dd0fd610fc2db74b7e211ad318c2f025d
delladwiagustin <160448812+delladwiagustin@users.noreply.github.com>
2024-04-29 10:26:22 +0700

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,1 +1,13 @@
-
+<script>
+$(document).ready(function(){
+    $.ajax({
+        url: 'toko_buku.json',
+        dataType: 'json',
+        success: function(data) {
+            $('#namaToko').text(data.nama_toko);
+            $('#pengarang').text(data.pengarang);
+            $('#alamat').text(data.alamat);
+        }
+    });
+});
+</script>
