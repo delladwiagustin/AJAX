44ea774c90a843b9e43a2b9407ba19dc6909e7e0
delladwiagustin <160448812+delladwiagustin@users.noreply.github.com>
2024-04-29 10:27:28 +0700

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
             $('#namaToko').text(data.nama_toko);
             $('#pengarang').text(data.pengarang);
             $('#alamat').text(data.alamat);
+            
+            var table = '<table border="1"><tr><th>Judul</th><th>Tahun Terbit</th><th>Jumlah Halaman</th></tr>';
+            $.each(data.koleksi_buku, function(key, value) {
+                table += '<tr>';
+                table += '<td>' + value.judul + '</td>';
+                table += '<td>' + value.tahun_terbit + '</td>';
+                table += '<td>' + value.jumlah_halaman + '</td>';
+                table += '</tr>';
+            });
+            table += '</table>';
+            $('#koleksiBuku').html(table);
         }
     });
 });
